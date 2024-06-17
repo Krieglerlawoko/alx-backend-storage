e85683e8b6744d6283b727ff6fadead14b38513e
Kreiglerlawoko <Kreiglerlawokojudge@gmail.com>
2024-06-17 11:05:12 +0000

    new'

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python3
-"""
-Return all students
-sorted by average score.
-"""
-
-from pymongo import MongoClient
+"""Top"""
 
 
 def top_students(mongo_collection):
     """
-    Return all students sorted by average score.
-    Parameters:
-    mongo_collection (Collection): The
-    MongoDB collection containing student records.
-    Returns:
-    list: A list of students sorted by
-    their average score in descending order.
+    returns all students sorted by average score
+    :return:
     """
-    return list(mongo_collection.aggregate([
-        {
-            "$project": {
-                "name": 1,
-                "averageScore": {"$avg": "$scores.score"}
-            }
-        },
-        {
-            "$sort": {"averageScore": -1}
-        }
-    ]))
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
