8f8667e2738217b595c2c983caf59e6dfc5f1467
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-19 14:14:05 +0000

    new

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-A module with tools for request caching and tracking.
+A module for caching HTTP requests and tracking access.
 """
 
 import redis
@@ -9,34 +9,42 @@ from functools import wraps
 from typing import Callable
 
 # Initialize a Redis client
-redis_store = redis.Redis()
+redis_client = redis.Redis()
 
-def data_cacher(method: Callable) -> Callable:
+
+def track_get_page(fn: Callable) -> Callable:
     """
-    Decorator that caches output of method.
-    Tracks number of times URL is accessed.
+    Decorator for get_page that:
+    - Checks if URL's data is cached
+    - Tracks how many times get_page is called
     """
-    @wraps(method)
+    @wraps(fn)
     def wrapper(url: str) -> str:
         """
-        Wrapper function for caching output.
-        Increments request count .caches result.
+        Wrapper that:
+        - Increments the access count for the URL
+        - Returns cached data if available
+        - Fetches and caches data if not available
         """
-        redis_store.incr(f'count:{url}')
-        cached_result = redis_store.get(f'result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-        
-        result = method(url)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
+        # Track the number of times the URL is accessed
+        redis_client.incr(f'count:{url}')
+        # Check if the URL's data is cached
+        cached_page = redis_client.get(url)
+        if cached_page:
+            return cached_page.decode('utf-8')
+        # Fetch the data and cache it
+        response = fn(url)
+        redis_client.setex(url, 10, response)
+        return response
+
     return wrapper
 
-@data_cacher
+
+@track_get_page
 def get_page(url: str) -> str:
     """
-    Fetches content of URL.caches response.
+    Makes an HTTP GET request to a given URL and returns the response text.
     """
     response = requests.get(url)
-    response.raise_for_status()  # Ensure handling HTTP errors
+    response.raise_for_status()
     return response.text
